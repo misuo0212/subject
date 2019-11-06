7c44703ba940a29572b7d066f5a5b6350dccbcce
qwer <qwer@naver.com>
2019-11-06 21:56:35 +0900

    add a print line

--- Class2/src/test2.java
+++ Class2/src/test2.java
@@ -3,6 +3,9 @@ public class test2 {
 
 	public static void main(String[] args) {
 		System.out.println("Wonkwang University SW Department");
+		System.out.println("1st Grade");
+		System.out.println("2nd Grade");
+		System.out.println("3rd Grade");
 	}
 
 }
