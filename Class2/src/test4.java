174e8b2c50780e0977eeb570e54f8c83516bc883
qwer <qwer@naver.com>
2019-11-20 01:07:50 +0900

    create project

--- Class2/src/test4.java
+++ Class2/src/test4.java
@@ -1,9 +1,8 @@
 
-public class test3 {
+public class test4 {
 
 	public static void main(String[] args) {
 		System.out.println("Add Subject Java Class file push");
-		System.out.println("start to game");
 	}
 
 }
